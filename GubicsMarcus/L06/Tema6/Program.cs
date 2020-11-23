db6cfa67ecd1eda527dc1615cf0a73c04bf5b17e
gubicsmarcus <71893794+gubicsmarcus@users.noreply.github.com>
2020-11-23 13:02:36 +0200

    Tema 6 update v2

--- GubicsMarcus/L06/Tema6/Program.cs
+++ GubicsMarcus/L06/Tema6/Program.cs
@@ -16,13 +16,20 @@ namespace Tema6
                      from authorAck in Domain.SendAckToReplyAuthor(2, 1, "test")
                      select (validReply, checkLanguageResult, ownerAck, authorAck);
 
+			var serviceProvider = new ServiceCollection()
+                .AddOperations(typeof(ValidateReplyAdapter).Assembly)
+                .AddTransient<IInterpreterAsync>(sp => new LiveInterpreterAsync(sp))
+                .BuildServiceProvider();
+
+            var interpreter = serviceProvider.GetService<IInterpreterAsync>();
+
+            var writeContext = new QuestionWriteContext(new List<int>() { 1, 2, 3 }, new List<int>() { 100, 101, 102 });
+            var result = await interpreter.Interpret(wf, writeContext);
 
             Console.WriteLine("Hello World!");
+     
         }
 
     }
 
-    internal interface IReplyPosted
-    {
-    }
 }
